efafd40b7d706e282e9e53e875a459432b4d834a
igzhang <676529483@qq.com>
2020-02-15 11:06:40 +0800

    包含基本json解析和copy功能

--- src/index.js
+++ src/index.js
@@ -1,12 +1,141 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Layout, Input, Row, Col, message } from "antd";
+import Inspector from 'react-json-inspector';
+import { AiOutlineGithub } from "react-icons/ai";
+import "react-json-inspector/json-inspector.css";
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const { Header, Content } = Layout;
+const { TextArea } = Input;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <Header>
+                        <Title />
+                        <GithubLink />
+                    </Header>
+                    <Content>
+                        <JsonContent />
+                    </Content>
+                </Layout>
+            </div>
+        )
+    }
+}
+
+class Title extends React.Component {
+    render() {
+        return (
+            <div className="title">
+                <span>
+                    Find Element Path in Json
+                </span>
+            </div>
+        )
+    }
+}
+
+class GithubLink extends React.Component {
+    render() {
+        return (
+            <div className="github">
+                <a href="http://www.baidu.com">
+                    <AiOutlineGithub />
+                </a>
+            </div>
+        )
+    }
+}
+
+class JsonContent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        const json_example = {"key1": 123, "key2": "it is a example"};
+        this.state = {
+            json_object: json_example,
+        };
+    }
+
+    onInputChange = (data) => {
+        this.setState({json_object: data})
+    }
+
+    render() {
+        return (
+            <div>
+                <Row gutter={16}>
+                    <Col span={12}>
+                        <JsonInput onInputChange={this.onInputChange} />
+                    </Col>
+                    <Col span={12}>
+                        <JsonOutput json_object={this.state.json_object} />
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
+
+class JsonInput extends React.Component {
+
+    onChange = (e) => {
+        let json_object;
+        try {
+            json_object = JSON.parse(e.target.value);
+        } catch(err) {
+            json_object = err.message;
+        }
+        this.props.onInputChange(json_object);
+    }
+
+    render() {
+        return (
+            <div>
+                <TextArea 
+                    allowClear 
+                    autoSize={{ minRows: 30 }} 
+                    placeholder="input json data" 
+                    onChange={this.onChange}
+                />
+            </div>
+        )
+    }
+}
+
+class JsonOutput extends React.Component {
+
+    validateQuery = (query) => {
+        return true
+    }
+
+    onClick = (copyData) => {
+        const container = document.createElement('textarea');
+        container.innerHTML = copyData.path;
+        document.body.appendChild(container);
+        container.select();
+        document.execCommand('copy');
+
+        message.success('copy path success!');
+        document.body.removeChild(container);
+    }
+
+    render() {
+        return (
+            <div>
+                <Inspector 
+                    data={this.props.json_object} 
+                    onClick={this.onClick}
+                    validateQuery={this.validateQuery}
+                />
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
